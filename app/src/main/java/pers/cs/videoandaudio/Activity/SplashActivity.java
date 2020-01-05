c9baa3c2b4ecdbae876df5f1535a1b8b24b9f3ec
chensen <1439071609@qq.com>
2020-01-05 22:18:36 +0800

    SplashActivity简单完成

--- app/src/main/java/pers/cs/videoandaudio/Activity/SplashActivity.java
+++ app/src/main/java/pers/cs/videoandaudio/Activity/SplashActivity.java
@@ -1,16 +1,75 @@
 package pers.cs.videoandaudio.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MotionEvent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import pers.cs.videoandaudio.MainActivity;
 import pers.cs.videoandaudio.R;
 
+/**
+ * @author chensen
+ *
+ * @time 2020/1/5  21:36
+ *
+ * @desc
+ *
+ * SplashActivity、WelcomeActivity、LaucherActivity
+ */
+
 public class SplashActivity extends AppCompatActivity {
+
+    private static final String TAG = SplashActivity.class.getSimpleName();
+    private Handler handler = new Handler();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_splash);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //注意：此方法在主线程中执行，Handler在哪个线程中new，它就在哪个线程执行。
+                startMainActivity();
+
+                Log.d(TAG, "run: 当前线程名称：" + Thread.currentThread().getName());
+            }
+        },2000);
+    }
+
+    //不设置单例模式，让其启动一次
+    private boolean isStartMain = false;
+    private void startMainActivity() {
+//        if(!isStartMain) {
+//            isStartMain = true;
+            Intent intent = new Intent(this, MainActivity.class);
+
+            startActivity(intent);
+
+            finish();
+//        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "run: onTouchEvent：" + event.getAction());
+        startMainActivity();
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        //防止刚启动时点击退出后，启动。
+        //因为handler没有移除消息，2秒后便启动MainActivity。
+        handler.removeCallbacksAndMessages(null);
+        super.onDestroy();
+
     }
 }
