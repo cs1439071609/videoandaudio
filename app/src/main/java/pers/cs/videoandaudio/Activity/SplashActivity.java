761c4248aff8e1cfc57590febe478a5a86af5371
chensen <1439071609@qq.com>
2020-01-05 20:38:36 +0800

    init

---
+++ app/src/main/java/pers/cs/videoandaudio/Activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package pers.cs.videoandaudio.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import pers.cs.videoandaudio.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
